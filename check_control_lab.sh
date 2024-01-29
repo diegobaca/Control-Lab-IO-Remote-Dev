0bb120773f9072659c1ffc66fd942070566af7b4
Diego Baca <58245015+diegobaca@users.noreply.github.com>
2024-01-29 10:00:10 -0800

    Update Install

--- check_control_lab.sh
+++ check_control_lab.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-# Fetch the IP address
-IP_ADDRESS=$(hostname -I | awk '{print $1}')
+# Check if controllabio-remote service is active
+if systemctl is-active --quiet controllabio-remote.service; then
+    # Fetch the IP address
+    IP_ADDRESS=$(hostname -I | awk '{print $1}')
 
-# Start coloring and print the message with the IP address
-printf "\033[1;32m" # Start coloring
-echo
-echo "********************************************************************************"
-echo "Control Lab IO Remote is now running."
-echo "Type $IP_ADDRESS:5001 on a browser to control your LEGO Interface B."
-echo "********************************************************************************"
-echo
-printf "\033[0m" # Reset text color back to default
+    # Start coloring and print the message with the IP address
+    printf "\033[1;32m" # Start coloring
+    echo
+    echo "********************************************************************************"
+    echo "Control Lab IO Remote is now running."
+    echo "Type $IP_ADDRESS:5001 on a browser to control your LEGO Interface B."
+    echo "********************************************************************************"
+    echo
+    printf "\033[0m" # Reset text color back to default
+else
+    # Start coloring and print the message that the service is not running
+    printf "\033[1;32m" # Start coloring
+    echo
+    echo "********************************************************************************"
+    echo "Control Lab IO Remote is not running."
+    echo "Type command 'startcontrollab' to restart the service."
+    echo "********************************************************************************"
+    echo
+    printf "\033[0m" # Reset text color back to default
+fi
